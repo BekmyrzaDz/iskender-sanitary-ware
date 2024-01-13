444198a7f4aab6533ee3802a77d81967cb845cf0
Bekmyrza <msn.10110922@gmail.com>
2024-01-13 15:19:14 +0600

    Implement the behavior of the plus and minus buttons when hovering

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react"
 import { Card } from "../Card/Card"
-import { Minus, Plus, favorite, activeFavorite } from "../../assets"
+import { favorite, activeFavorite } from "../../assets"
 import styles from "./Product.module.scss"
 
 interface ISalesHits {
@@ -47,7 +47,7 @@ export const Product: FC<ISalesHits> = ({
           />
         ) : (
           <img
-            className={styles.favorite}
+            className={styles.activeFavorite}
             src={favorite}
             alt="Favorite icon"
             onClick={handleActive}
@@ -64,11 +64,31 @@ export const Product: FC<ISalesHits> = ({
         </div>
         <div className={styles.quantityWrapper}>
           <div className={styles.minus} onClick={handleSalesHitQuantityMinus}>
-            <img className={styles.minusImg} src={Minus} alt="Minus icon" />
+            <svg
+              className={styles.minusImg}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="inherit">
+                <path d="M19 11H5C4.73478 11 4.48043 11.1054 4.29289 11.2929C4.10536 11.4804 4 11.7348 4 12C4 12.2652 4.10536 12.5196 4.29289 12.7071C4.48043 12.8946 4.73478 13 5 13H19C19.2652 13 19.5196 12.8946 19.7071 12.7071C19.8946 12.5196 20 12.2652 20 12C20 11.7348 19.8946 11.4804 19.7071 11.2929C19.5196 11.1054 19.2652 11 19 11Z" />
+              </g>
+            </svg>
           </div>
           <p className={styles.quantity}>{salesHitQuantity}</p>
           <div className={styles.plus} onClick={handleSalesHitQuantityPlus}>
-            <img className={styles.plusImg} src={Plus} alt="Plus icon" />
+            <svg
+              className={styles.plus}
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g fill="inherit">
+                <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+              </g>
+            </svg>
           </div>
         </div>
       </div>
